6da25f21bf8b4bdda97647b6665b6fdff57a6cbf
Miguelote <al066382@uacam.mx>
2024-12-06 00:55:08 -0600

    cambios

--- views/client/PedidosView.php
+++ views/client/PedidosView.php
@@ -85,13 +85,12 @@ foreach ($pedidosAgrupados as $statusPa => $pedidosStatus) {
     <section id="pagos" class="orders mt-3 w-2/3">
         <div class="w-full">
             <header>
-                <h2 class="ml-4 text-2xl font-semibold">Listos</h2>
+                <h2 class="ml-4 text-2xl font-bold">Listos</h2>
             </header>
             <div class="relative overflow-hidden">
                 <ul id="pedidos-pagados">
                     <?php foreach ($pedidosPagados as $pedido): ?>
-                        <li class="p-4 rounded-lg pedido-item" data-nombre="<?php echo htmlspecialchars($pedido['nombreC']); ?>">
-                            <h3 class="text-lg font-bold">Pedido de: <?php echo htmlspecialchars($pedido['nombreC']); ?></h3>
+                        <li class="p-4 rounded-md shadow-md pedido-item" data-nombre="<?php echo htmlspecialchars($pedido['nombreC']); ?>">
                             <p>Fecha: <?php echo date("d-m-Y", strtotime($pedido['fecha'])); ?></p>
                             <p>Hora: <?php echo $pedido['hora']; ?></p>
                             <p>Total: $<?php echo number_format($pedido['total'], 2); ?></p>
@@ -103,24 +102,39 @@ foreach ($pedidosAgrupados as $statusPa => $pedidosStatus) {
 
             <!-- Mostrar los pedidos En Proceso -->
             <header>
-                <h2 class="ml-4 text-2xl font-semibold">En Proceso</h2>
+                <h2 class="ml-4 text-2xl font-bold">En Proceso</h2>
             </header>
             <div class="relative overflow-hidden">
-                <ul id="pedidos-en-proceso">
+                <ul id="pedidos-en-proceso" class="grid grid-cols-4">
                     <?php foreach ($pedidosEnProceso as $pedido): ?>
-                        <li class="p-4 rounded-lg pedido-item" data-nombre="<?php echo htmlspecialchars($pedido['nombreC']); ?>">
-                            <h3 class="text-lg font-bold">Pedido de: <?php echo htmlspecialchars($pedido['nombreC']); ?></h3>
-                            <p>Fecha: <?php echo date("d-m-Y", strtotime($pedido['fecha'])); ?></p>
-                            <p>Hora: <?php echo $pedido['hora']; ?></p>
-                            <p>Total: $<?php echo number_format($pedido['total'], 2); ?></p>
-                            <p>Status de Pedido:
-                                <?php echo match ($pedido['statusPe']) {
-                                    1 => 'Pendiente',
-                                    2 => 'Preparando',
-                                    3 => 'Listo',
-                                    default => 'Estado desconocido',
-                                }; ?>
-                            </p>
+                        <li class="p-4 col-span-1 border rounded-md shadow-lg pedido-item" data-nombre="<?php echo htmlspecialchars($pedido['nombreC']); ?>">
+                            <div class="flex flex-start align-middle gap-3">
+                                <svg width="22" height="22" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M20 10V7C20 5.89543 19.1046 5 18 5H6C4.89543 5 4 5.89543 4 7V10M20 10V19C20 20.1046 19.1046 21 18 21H6C4.89543 21 4 20.1046 4 19V10M20 10H4M8 3V7M16 3V7" stroke="#000000" stroke-width="2" stroke-linecap="round"></path> <rect x="6" y="12" width="3" height="3" rx="0.5" fill="#000000"></rect> <rect x="10.5" y="12" width="3" height="3" rx="0.5" fill="#000000"></rect> <rect x="15" y="12" width="3" height="3" rx="0.5" fill="#000000"></rect> </g></svg>
+                                <p>Fecha: <?php echo date("d-m-Y", strtotime($pedido['fecha'])); ?></p>
+                            </div>
+                            <div class="flex align-middle gap-3">
+                                <svg fill="#000000" width="22" height="22" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M0 8c0 4.418 3.59 8 8 8 4.418 0 8-3.59 8-8 0-4.418-3.59-8-8-8-4.418 0-8 3.59-8 8zm2 0c0-3.307 2.686-6 6-6 3.307 0 6 2.686 6 6 0 3.307-2.686 6-6 6-3.307 0-6-2.686-6-6zm5 1h5V7H9V4H7v5z" fill-rule="evenodd"></path> </g></svg>
+                                <p>Hora: <?php echo $pedido['hora']; ?></p>
+                            </div>
+                            <div class="flex align-middle gap-3">
+                                <svg width="22" height="22" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M14 14H17M14 10H17M9 9.5V8.5M9 9.5H11.0001M9 9.5C7.20116 9.49996 7.00185 9.93222 7.0001 10.8325C6.99834 11.7328 7.00009 12 9.00009 12C11.0001 12 11.0001 12.2055 11.0001 13.1667C11.0001 13.889 11.0001 14.5 9.00009 14.5M9.00009 14.5L9 15.5M9.00009 14.5H7.0001M6.2 19H17.8C18.9201 19 19.4802 19 19.908 18.782C20.2843 18.5903 20.5903 18.2843 20.782 17.908C21 17.4802 21 16.9201 21 15.8V8.2C21 7.0799 21 6.51984 20.782 6.09202C20.5903 5.71569 20.2843 5.40973 19.908 5.21799C19.4802 5 18.9201 5 17.8 5H6.2C5.0799 5 4.51984 5 4.09202 5.21799C3.71569 5.40973 3.40973 5.71569 3.21799 6.09202C3 6.51984 3 7.07989 3 8.2V15.8C3 16.9201 3 17.4802 3.21799 17.908C3.40973 18.2843 3.71569 18.5903 4.09202 18.782C4.51984 19 5.07989 19 6.2 19Z" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg>
+                                <p>Total: $<?php echo number_format($pedido['total'], 2); ?></p>
+                            </div>
+                            <div class="flex align-middle gap-3">
+                                <svg width="22" height="22" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"><path d="M9.9898 22.55C9.9398 22.55 9.8898 22.55 9.8398 22.53C6.0098 21.75 2.8998 18.93 1.7398 15.19C1.6198 14.79 1.8398 14.37 2.2298 14.25C2.6298 14.13 3.0498 14.35 3.1698 14.74C4.1698 17.96 6.8398 20.38 10.1398 21.05C10.5498 21.13 10.8098 21.53 10.7198 21.94C10.6498 22.3 10.3298 22.55 9.9898 22.55Z" fill="#000000"></path><path d="M21.9502 11.73C21.5702 11.73 21.2402 11.44 21.2002 11.06C20.7202 6.32 16.7602 2.75 12.0002 2.75C7.23022 2.75 3.28022 6.32 2.80022 11.05C2.76022 11.46 2.40022 11.77 1.98022 11.72C1.57022 11.68 1.27022 11.31 1.31022 10.9C1.87022 5.4 6.47022 1.25 12.0002 1.25C17.5402 1.25 22.1402 5.4 22.6902 10.9C22.7302 11.31 22.4302 11.68 22.0202 11.72C22.0002 11.73 21.9702 11.73 21.9502 11.73Z" fill="#000000"></path><path d="M14.0101 22.55C13.6601 22.55 13.3501 22.31 13.2801 21.95C13.2001 21.54 13.4601 21.15 13.8601 21.07C17.1401 20.4 19.8101 18 20.8201 14.8C20.9401 14.4 21.3701 14.18 21.7601 14.31C22.1601 14.43 22.3701 14.86 22.2501 15.25C21.0701 18.97 17.9701 21.76 14.1601 22.54C14.1101 22.54 14.0601 22.55 14.0101 22.55Z" fill="#000000"></path></g></svg>
+                                <p>Status de Pedido:
+                                    <?php echo match ($pedido['statusPe']) {
+                                        1 => 'Pendiente',
+                                        2 => 'Preparando',
+                                        3 => 'Listo',
+                                        default => 'Estado desconocido',
+                                    }; ?>
+                                </p>
+                            </div>
+
+
+
+
                         </li>
                     <?php endforeach; ?>
 
